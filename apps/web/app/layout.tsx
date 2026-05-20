5f3c169c560919dc39b8d99caeb2fb74e7aad48a
Azahari Zaman <azaharizaman@gmail.com>
2026-05-21 03:10:12 +0800

    CHange to dark mode temporarily

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="h-full antialiased">
+    <html lang="en" className="h-full antialiased dark">
       <body className="flex min-h-full flex-col">
         <AppProviders>{children}</AppProviders>
       </body>
